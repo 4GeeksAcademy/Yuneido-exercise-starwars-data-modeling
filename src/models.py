834c0694fa99e2e350df3083943cf4d355452c12
Yuneido Chacin <146007178+yjchf@users.noreply.github.com>
2024-10-15 03:33:14 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -1,32 +1,63 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from datetime import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, UniqueConstraint, DateTime, func
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Usuario(Base):
+    __tablename__ = 'usuario'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(50))
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(50))
+    password = Column(String(100))
+    join_date = Column(DateTime, server_default=func.now())
 
-    def to_dict(self):
-        return {}
+
+class favList(enum.Enum):
+    CHARACTERS = 'Character'
+    PLANETS = 'Planet'
+
+class Characters(Base):
+    __tablename__ = 'characters'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    description = Column(String(200))
+    gender = Column(String(50))
+    eye_color = Column(String(50))
+    hair_color = Column(String(50))
+    height = Column(String(50))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    terrain = Column(String(100))
+    population = Column(Integer)
+    climate = Column(String(50))
+
+
+class Favorites(Base):
+    __tablename__='favorites'
+
+    id= Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('usuario.id'))
+    fav_type = Column('Tipo', Enum(favList))
+    fav_id = Column(Integer, nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'fav_id', 'Tipo', name='uq_user_type_id'),
+    )
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
